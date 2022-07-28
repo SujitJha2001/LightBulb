40151396b626bb21d134f84ab15709428b6429ee
Sujit Jha <SujitJha@example.com>
2022-07-28 20:13:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function bulbOn(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","images/bulbon.PNG")
+}
+
+function bulbOff(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","images/bulboff.PNG")
+}
